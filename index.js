33b5d83bcb9fe790462da969bc856d3bfdf4f1d5
DILIP SINGH <146862996+thedilipsinghh@users.noreply.github.com>
2026-02-11 21:53:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const jsonServer = require('json-server')
 const cors = require('cors')
 const path = require('path')
 
-//  very important 
+
+
 const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults({
